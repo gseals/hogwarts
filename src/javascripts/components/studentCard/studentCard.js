9a5b1d754d6e679e3707c928ff88a64c9e47f100
Gabriel Seals <sealsgj@gmail.com>
2019-10-05 11:46:29 -0500

    cards printing and styles working

---
+++ src/javascripts/components/studentCard/studentCard.js
@@ -0,0 +1,16 @@
+import './studentCard.scss';
+
+const createStudentCard = (student) => {
+  const domString = `
+  <li class="student-card">
+  <div class="img-holder">
+  <img src=${student.picture}>
+  </div>
+  <h2>${student.name}</h2>
+  </li>
+  `;
+  return domString;
+  // util.printToDom();
+};
+
+export default { createStudentCard };
